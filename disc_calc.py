7a55a64705bee395d599e47044169a25e430f40d
Andrew <caolegario@gmail.com>
2014-02-06 19:37:54 -0800

    excessive discount

--- disc_calc.py
+++ disc_calc.py
@@ -1,10 +1,14 @@
 class DiscountCalculator(object):
 	def calculate (self, total, discount_amount, discount_type):
 		if discount_type == 'percent':
+			if discount_amount > 100:
+				raise ValueError ("Percentage discount cannot exceed 100%")
 			percentage_discount = float(discount_amount) / 100
 			discount = float(total) * percentage_discount
-		else:
+		elif discount_type == 'dollar':
+			if discount_amount > total:
+				raise ValueError ("Discount can't exceed total price.")
 			discount = discount_amount
+		else:
+			raise ValueError("Invalid discount type")
 		return discount
-
-
