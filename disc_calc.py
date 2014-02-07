a00a2dbdd8e3a191349ef5b011edd7ede9d51fb8
Andrew <caolegario@gmail.com>
2014-02-06 17:11:10 -0800

    limited functionality

--- disc_calc.py
+++ disc_calc.py
@@ -1,3 +1,5 @@
 class DiscountCalculator(object):
-	def calculate (self, total, discount_amout, discount_type):
-		return 10.0
+	def calculate (self, total, discount_amount, discount_type):
+		percentage_discount = float(discount_amount) / 100
+		discount = float(total) * percentage_discount
+		return discount
